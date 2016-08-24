88fc152cf5edef76e058163b8623af11566dbfd6
Priyanka Dwivedi <priyanka.kochhar3@gmail.com>
2016-08-24 11:47:12 -0400

    Add files via upload

---
+++ outliers/enron_outliers.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+
+import pickle
+import sys
+import matplotlib.pyplot
+import os
+os.chdir(r"C:\Users\s6324900\Documents\ud120projects\tools")
+
+from feature_format import featureFormat, targetFeatureSplit
+
+
+### read in data dictionary, convert to numpy array
+data_dict = pickle.load( open("../final_project/final_project_dataset.pkl", "r") )
+features = ["salary", "bonus"]
+data = featureFormat(data_dict, features)
+
+
+### your code below
+for point in data:
+    salary = point[0]
+    bonus = point[1]
+    matplotlib.pyplot.scatter( salary, bonus )
+
+matplotlib.pyplot.xlabel("salary")
+matplotlib.pyplot.ylabel("bonus")
+matplotlib.pyplot.show()
+
+
---
+++ outliers/outlier_cleaner.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+
+
+def outlierCleaner(predictions, ages, net_worths):
+    """
+        Clean away the 10% of points that have the largest
+        residual errors (difference between the prediction
+        and the actual net worth).
+
+        Return a list of tuples named cleaned_data where 
+        each tuple is of the form (age, net_worth, error).
+    """
+    
+    cleaned_data = []
+    
+    ### your code goes here
+
+    
+    errors = (net_worths-predictions)**2
+    cleaned_data =zip(ages,net_worths,errors)
+    cleaned_data.sort(key=lambda tup: tup[2])
+    return cleaned_data[:81]
+
+    #cleaned_data = sorted(cleaned_data,key=lambda x:x[2][0], reverse=True)
+    #limit = int(len(net_worths)*0.1)
+    #return cleaned_data[limit:]
+    
+
